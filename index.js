c3b5fad4e86bbd5515bb6362eb3bbe9163afe489
Tom Keogh <thomas.keogh@me.com>
2018-03-15 11:03:09 +0000

    Setting up for deployment

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.static(`${__dirname}/public`));
+
+app.listen(port, () => console.log(`Up and running on port ${port}`));
