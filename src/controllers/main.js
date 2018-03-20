bdba5e07f8152e65afc430b0789a0200c329e18e
Tom Keogh <thomas.keogh@me.com>
2018-03-20 10:50:00 +0000

    Burger menu now working using ng-class and ng-click

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,8 +1,19 @@
-MainCtrl.$inject = ['$rootScope','User','$auth', '$state', '$timeout'];
+MainCtrl.$inject = ['$rootScope','User','$auth', '$state', '$timeout', '$scope'];
 
-function MainCtrl($rootScope, User,$auth, $state, $timeout) {
+function MainCtrl($rootScope, User,$auth, $state, $timeout, $scope) {
   this.flashMessage = null;
   this.isAuthenticated = $auth.isAuthenticated;
+
+  // This changeClass function enables the mobile burger menu in index.html
+  $scope.class = '';
+
+  $scope.changeClass = function(){
+    if ($scope.class === 'is-active')
+      $scope.class = '';
+    else
+      $scope.class = 'is-active';
+  };
+  //
 
   function logout(){
     $rootScope.$broadcast('flashMessage', {
