fab16863f3ef40d793af54b01ef36083c7eeebb4
AbiHill <abihill8@gmail.com>
2018-03-20 14:56:57 +0000

    current location now populates as the origin if the button is click and home populates in the destination if button clicked. disablled buttons if not logged in

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,8 +1,9 @@
 MainCtrl.$inject = ['$rootScope','User','$auth', '$state', '$timeout'];
 
-function MainCtrl($rootScope, User,$auth, $state, $timeout) {
-  this.flashMessage = null;
-  this.isAuthenticated = $auth.isAuthenticated;
+function MainCtrl($rootScope, User, $auth, $state, $timeout){
+  const vm = this;
+  vm.flashMessage = null;
+  vm.isAuthenticated = $auth.isAuthenticated;
 
   function logout(){
     $rootScope.$broadcast('flashMessage', {
@@ -14,52 +15,13 @@ function MainCtrl($rootScope, User,$auth, $state, $timeout) {
   }
 
   $rootScope.$on('flashMessage', (e, data) => {
-    this.flashMessage = data;
-    $timeout(() => this.flashMessage = null, 4000);
+    vm.flashMessage = data;
+    $timeout(() => vm.flashMessage = null, 4000);
   });
 
-  this.logout = logout;
-  // add in the broken down function here and refer to what I've called it on the home page view
-  this.userCurrentAddress = 'Hello';
-  console.log(this.userCurrentAddress);
+  vm.logout = logout;
 
-  function userCurrentPosition(){
-    //ths works below!
-    // navigator.geolocation.getCurrentPosition(pos => console.log(pos.coords.latitude, pos.coords.longitude
-    // ), err => console.log(err));
 
-    //trialling something new:
-    navigator.geolocation.getCurrentPosition(pos => {
-      const userCurrentLat = pos.coords.latitude;
-      const userCurrentLng = pos.coords.longitude;
-      console.log(userCurrentLat, userCurrentLng );
-      //I now need to convert the lat and long in to an origin
-      const latLng = {lat: userCurrentLat, lng: userCurrentLng};
-      console.log(latLng);
-
-      const geocoder = new google.maps.Geocoder;
-      geocoder.geocode({'location': latLng}, function(results, status) {
-        if (status === 'OK') {
-          if (results[0]) {
-            this.userCurrentAddress = results[0].formatted_address;
-
-            console.log('current address has changed to:' + this.userCurrentAddress);
-          } else {
-            console.log('No results found');
-          }
-        } else {
-          console.log('Geocoder failed due to: ' + status);
-        }
-      });
-      //then I need to input the origin in the the google-maps directive
-    }, err => {
-      console.warn(err.code, err.message);
-    });
-
-    //here I want to save the lat and lng as seperate variales.
-    //then I want to save them as the value in the form with an ng-m
-  }
-  this.userCurrentPosition = userCurrentPosition;
 
 }
 
--- src/controllers/pages/home.js
+++ src/controllers/pages/home.js
@@ -1,13 +1,74 @@
-function PagesHomeCtrl() {
-  this.origin = 'St Pauls, London';
-  this.destination = 'Oxford Street, London';
-  this.travelMode = '';
+/* global google */
+PagesHomeCtrl.$inject = ['$scope', '$auth', 'User'];
+
+function PagesHomeCtrl($scope, $auth, User) {
+
+  const vm = this;
+  vm.origin = 'St Pauls, London';
+  vm.destination = 'Oxford Street, London';
+  vm.travelMode = '';
 
   function setTravelMode(mode) {
-    this.travelMode = mode;
+    vm.travelMode = mode;
   }
 
-  this.setTravelMode = setTravelMode;
+  //CURRENT LOCATION FUNCTION
+  vm.userCurrentAddress = 'Your Current location will appear here';
+  console.log(vm.userCurrentAddress);
+
+  function userCurrentPosition(){
+    //The below function locates your current position in to lat and lng variables.
+    navigator.geolocation.getCurrentPosition(pos => {
+      const userCurrentLat = pos.coords.latitude;
+      const userCurrentLng = pos.coords.longitude;
+      //The below changes them in to an object ready for convering them in to an address string
+      const latLng = {lat: userCurrentLat, lng: userCurrentLng};
+      //The below uses the latLng object and finds the formatted_address
+      const geocoder = new google.maps.Geocoder;
+      geocoder.geocode({'location': latLng}, function(results, status) {
+        if (status === 'OK') {
+          if (results[0]) {
+            vm.userCurrentAddress = results[0].formatted_address;
+            vm.origin = results[0].formatted_address;
+            $scope.$apply();
+
+            console.log('This is your current location:' + vm.userCurrentAddress);
+          } else {
+            console.log('No results found');
+          }
+        } else {
+          console.log('Geocoder failed due to: ' + status);
+        }
+      });
+      //then I need to input the origin in the the google-maps directive
+    }, err => {
+      console.log(err.code, err.message);
+    });
+
+    //here I want to save the lat and lng as seperate variales.
+    //then I want to save them as the value in the form with an ng-m
+  }
+  vm.userCurrentPosition = userCurrentPosition;
+  vm.setTravelMode = setTravelMode;
+
+
+  //HOME LOCATION FUNCTION // I'M HEEEERRRRREEEEE!
+  //abi testing getting the home address
+  function pullUserHome(){
+    vm.userHome = '';
+    const payload = $auth.getPayload();
+    User.findById(payload.sub)
+      .then(output => {
+        vm.userHome = output.data.home;
+        vm.destination = output.data.home;
+        console.log(vm.userHome);
+      });
+  }
+
+  this.pullUserHome = pullUserHome;
+
+  //*****************************************************
+
 }
 
 export default PagesHomeCtrl;
