82f73bcfbfbbf038cecff06b06e62897ce24593b
AbiHill <abihill8@gmail.com>
2018-03-21 16:55:47 +0000

    required field on registration for name field

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -4,6 +4,7 @@ function AuthRegisterCtrl($auth, $state){
   this.user = {};
 
   function handleSubmit(){
+    if(this.form.$invalid) return false;
     $auth.signup(this.user)
       .then(() => $state.go('authLogin'));
   }
