28ae1f4fe825ff1b314f071905de3997111b5cae
tjk3o <thomas.keogh@me.com>
2018-04-30 09:50:06 +0100

    Added missing chevron image back in and commented out config files

--- config/router.js
+++ config/router.js
@@ -1,4 +1,6 @@
+// Router adds routing APIs like .get .params route etc.
 const router = require('express').Router();
+// auth contains functions we have created to allow a user to register, login, etc.
 const auth = require('../controllers/auth');
 
 router.post('/register', auth.register);
@@ -7,6 +9,7 @@ router.post('/login', auth.login);
 router.get('/users/:id', auth.show);
 router.put('/users/:id', auth.update);
 
+// This final route catches unrecognised requests and returns a 404
 router.route('/*')
   .all((req, res) => res.status(404).json({ message: 'Not found' }));
 
